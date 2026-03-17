e4c14436dd15dbee6948c8081d61f96d0c34416e
EuCaue <souzacaue@proton.me>
2026-03-16 22:59:57 -0300

    feat: change header title

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,7 +4,7 @@ import { MD3LightTheme, Provider as PaperProvider } from "react-native-paper";
 export default function RootLayout() {
   return (
     <PaperProvider theme={MD3LightTheme}>
-      <Stack />
+      <Stack screenOptions={{ title: "Auto Volume", headerTitleAlign: "center" }} />
     </PaperProvider>
   );
 }
